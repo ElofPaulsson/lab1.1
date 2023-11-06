59cc5415633e53a8ffa64e001313a9697ced9e88
ElofPaulsson <elof.p@icloud.com>
2023-11-06 13:45:26 +0100

    H

---
+++ test/test.java
@@ -0,0 +1,36 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+
+
+public class test {
+
+
+    @Test
+
+    public void testmethodmove(){
+        test leoset = new test();
+        leoset.
+
+
+        }
+
+
+
+
+
+
+        public void testmethodsize(){
+            ARSet leoset = new ARSet();
+            leoset.add(5);
+            leoset.add(3);
+            leoset.add(4);
+            leoset.add(8);
+            assertEquals(3, leoset.size());
+
+
+
+
+        }
+
+}
